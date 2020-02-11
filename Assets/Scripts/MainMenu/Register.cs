00dd88e5ef8d141663f3393b843804e502384a7b
crannnabethadh <crannnabethadh@outlook.com>
2020-02-11 08:49:05 +0100

    Start

---
+++ Assets/Scripts/MainMenu/Register.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+using System.Text;
+using System.Collections;
+
+public class Register : MonoBehaviour
+{
+    // Cached references
+    public InputField emailInputField;
+    public InputField passwordInputField;
+    public InputField confirmPasswordInputField;
+    public Button registerButton;
+    public Text messageBoardText;
+    public Player playerManager;
+
+    string httpServer;
+
+    private void Start()
+    {
+        httpServer = playerManager.GetHttpServer();
+    }
+
+    public void OnRegisterButtonClick()
+    {
+        
+    }
+
+    private void RegisterNewUser()
+    {
+        if (string.IsNullOrEmpty(emailInputField.text))
+        {
+            throw new NullReferenceException("Email can't be void");
+        }
+        else if (string.IsNullOrEmpty(passwordInputField.text))
+        {
+            throw new NullReferenceException("Password can't be void");
+        }
+        else if (passwordInputField.text != confirmPasswordInputField.text)
+        {
+            throw new Exception("Passwords don't match");
+        }
+
+        AspNetUserRegister newUser = new AspNetUserRegister();
+        newUser.Email = emailInputField.text;
+        newUser.Password = passwordInputField.text;
+        newUser.ConfirmPassword = confirmPasswordInputField.text;
+
+
+
+
+    }
+
+}
